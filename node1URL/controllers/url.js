309da4b40b50bc9fb42467074783ba3c525131a5
suruchi Bharti <suruchi.bharti@intimetec.com>
2026-02-13 13:08:45 +0530

    change url to short url

---
+++ node1URL/controllers/url.js
@@ -0,0 +1,28 @@
+const {nanoid} = require("nanoid");
+const URL = require("../models/url.js");
+async function handleGenerateNewShortURL(req,res){
+    const body = req.body;
+    if(!body.url ) return res.status(400).json({ error: "url is requried"})
+    const shortID = nanoid(8);
+
+    await URL.create({
+        shortId: shortID,
+        redirectURL: body.url,
+        visitHistory: [],
+    })
+    return res.json({id: shortID});
+}
+
+async function handleGetAnalytics(req,res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({shortId});
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+    });
+}
+
+module.exports = {
+    handleGenerateNewShortURL,
+    handleGetAnalytics,
+}
